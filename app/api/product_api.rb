1f6cb77ceb4d4ec7d47f1c06ed230949eda5c2b2
xdite <xuite.joke@gmail.com>
2016-05-18 23:09:28 +0800

    keychain design

--- app/api/product_api.rb
+++ app/api/product_api.rb
@@ -9,7 +9,16 @@ module ProductAPI
 
     get :list do
       @products = Product.all
+    end
 
+    resource :add_to_cart do 
+      params do
+        requires :token, type: String, desc: "API Token for auth"
+      end
+
+      post do 
+        @product = Product.find(params[:product_id])
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+ 
   mount ProductAPI::API => '/'
   mount UserAPI::API => '/'
 
