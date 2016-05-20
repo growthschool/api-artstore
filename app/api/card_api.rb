2bf318c287a7d17e67413cb39bce1d0c5069e701
xdite <xuite.joke@gmail.com>
2016-05-20 16:54:04 +0800

    add number

--- app/api/card_api.rb
+++ app/api/card_api.rb
@@ -7,8 +7,9 @@ module CardAPI
     
     params do
       requires :token, type: String, desc: "API Token for auth"
+     # requires :number, type: String, desc: "CreditCard Number"
     end
-\
+
 
     post do 
       @card = CreditCard.last
